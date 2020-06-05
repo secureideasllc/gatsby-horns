c9bc74d0f8d335e023aabe2fc7ae85f45fb16dfb
Kevin Johnson <kevin@kashyyyk.local>
2020-06-05 08:15:43 -0400

    Started...

--- src/horns.js
+++ src/horns.js
@@ -1,1 +1,10 @@
 // Horns.js: Helmet Replacement
+import React from "react";
+import PropTypes from "prop-types";
+import withSideEffect from "react-side-effect";
+import isEqual from "react-fast-compare";
+
+const Horns = Component =>
+    class HornWrapper extends React.Component {
+
+    }
